73999d8300931062c72f57b0091c4460619711a6
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-04-09 22:24:20 -0700

    mmu can now burst particles

--- Assets/Andrew/Scripts/MMUEffects.cs
+++ Assets/Andrew/Scripts/MMUEffects.cs
@@ -6,27 +6,31 @@ public class MMUEffects : MonoBehaviour {
     // Start is called before the first frame update
     private ParticleSystem ps;
     private GameObject player;
+    private PlayerMoveV2 move;
     private SpriteRenderer sr;
     private AudioSource Asource;
     void Start() {
         ps = GetComponent<ParticleSystem>();
         player = GameObject.Find("Player");
+        move = player.GetComponent<PlayerMoveV2>();
         sr = player.GetComponent<SpriteRenderer>();
         Asource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
-        Vector3 pos = transform.localPosition;
-        Vector3 coneRot = transform.eulerAngles;
-        if (sr.flipX) {
-            transform.eulerAngles = new Vector3(coneRot.x, 180, coneRot.z);
-            transform.localPosition = new Vector3(-.3f, pos.y, pos.z);
-        } else {
-            transform.eulerAngles = new Vector3(coneRot.x, 0, coneRot.z);
-            transform.localPosition = new Vector3(.3f,pos.y,pos.z);
-        }
-        if (Input.GetAxis("Horizontal") + Input.GetAxis("Vertical") > .01 && !Asource.isPlaying) {
+        if ((move.horizTimer < .1 || move.vertTimer < .1) && !Asource.isPlaying) {
+            Vector3 pos = transform.localPosition;
+            Vector3 coneRot = transform.eulerAngles;
+            if (sr.flipX) {
+                transform.eulerAngles = new Vector3(coneRot.x, 180, coneRot.z);
+                transform.localPosition = new Vector3(-.3f, pos.y, pos.z);
+            }
+            else {
+                transform.eulerAngles = new Vector3(coneRot.x, 0, coneRot.z);
+                transform.localPosition = new Vector3(.3f, pos.y, pos.z);
+            }
+            ps.Play();
             Asource.Play();
         }
     }
