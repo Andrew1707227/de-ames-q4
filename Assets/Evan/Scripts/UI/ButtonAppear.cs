cec0aec6ef15bcdd604eb26d0cdd39c0300382c2
ChumaDaCabbage <1707224@Mukilteo.wednet.edu>
2021-04-15 16:59:53 -0700

    Main Menu set up

---
+++ Assets/Evan/Scripts/UI/ButtonAppear.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonAppear : MonoBehaviour
+{
+    public GameObject sceneChangerObject;
+
+    Vector2 bSize;
+    Vector2 bPos;
+    bool done = false;
+
+    Image i;
+    RectTransform rt;
+    SceneChanger sC;
+
+    private void Awake()
+    {
+        rt = gameObject.GetComponent<RectTransform>();
+        i = gameObject.GetComponent<Image>();
+        sC = sceneChangerObject.GetComponent<SceneChanger>();
+
+        bSize = rt.sizeDelta;
+        bPos = rt.anchoredPosition;
+        i.enabled = false;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(openButton());
+    }
+
+    private void Update()
+    {
+        if (!done && sC.leaving)
+        {
+            done = true;
+            StartCoroutine(closeButton());
+        }
+    }
+
+    private IEnumerator closeButton()
+    {
+        rt.sizeDelta = bSize;
+        rt.anchoredPosition = bPos;
+
+        for (int i = (int)bSize.x; i >= 5; i -= 40)
+        {
+            rt.sizeDelta = new Vector2(i, bSize.y);
+            yield return new WaitForFixedUpdate();
+        }
+        rt.sizeDelta = new Vector2(5, bSize.y);
+        yield return new WaitForSeconds(.2f);
+
+        for (int i = (int)bPos.y; i >= bPos.y - 100; i -= 12)
+        {
+            rt.anchoredPosition = new Vector2(bPos.x, i);
+            yield return new WaitForFixedUpdate();
+        }
+        rt.anchoredPosition = new Vector2(bPos.x, bPos.y - 100);
+        i.enabled = false;
+    }
+
+    public IEnumerator openButton()
+    {
+        i.enabled = true;
+        rt.sizeDelta = new Vector2(5, bSize.y);
+        rt.anchoredPosition = new Vector2(bPos.x, bPos.y - 100);
+
+        for (int i = (int)(bPos.y - 100); i <= bPos.y; i += 12)
+        {
+            rt.anchoredPosition = new Vector2(bPos.x, i);
+            yield return new WaitForFixedUpdate();
+        }
+        rt.anchoredPosition = bPos;
+        yield return new WaitForSeconds(.2f);
+
+        for (int i = 5; i <= bSize.x; i += 40)
+        {
+            rt.sizeDelta = new Vector2(i, bSize.y);
+            yield return new WaitForFixedUpdate();
+        }
+        rt.sizeDelta = bSize;
+    }
+
+}
---
+++ Assets/Evan/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public bool leaving;
+
+    //Trasititons to title scene
+    public void toTitle()
+    {
+        leaving = true;
+        StartCoroutine(waiter("TitleScene"));
+    }
+
+    //Trasititons to game start scene
+    public void toGame()
+    {
+        leaving = true;
+        StartCoroutine(waiter("Level"));
+    }
+
+    //Trasititons to credits scene
+    public void toCredits()
+    {
+        leaving = true;
+        StartCoroutine(waiter("CreditsScene"));
+    }
+
+    public IEnumerator waiter(string location)
+    {
+        yield return new WaitForSeconds(0.8f);
+        SceneManager.LoadScene(location);
+    }
+}
