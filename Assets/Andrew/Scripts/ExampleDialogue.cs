b050c2b64e915cd5471eb5cae234aa8f5ee3856d
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-04-21 21:45:48 -0700

    Update ExampleDialogue.cs, TextScroller.cs.meta, and 2 more files...

--- Assets/Andrew/Scripts/ExampleDialogue.cs
+++ Assets/Andrew/Scripts/ExampleDialogue.cs
@@ -13,7 +13,7 @@ public class ExampleDialogue : MonoBehaviour {
         yield return new WaitForSeconds(1);
         //Type <d> at the start in order to use the distored audio (typing nothing uses the default)
         StartCoroutine(textScroller.RunText(new string[] { "Hello there! My name is T.O.D.D.", "I've been in this slimy worm for a long time,", 
-            "so sometimes ill malfunction and says things like", "<d>You're trash."}));
+            "and sometimes I'll malfunction and say things like,", "<d>You're trash."}));
         yield return new WaitUntil(() => textScroller.isTextFinished());
         StartCoroutine(textScroller.RunText(new string[] { "<d>Stings doesn't it?", "But what was I gonna say?","Oh yes, Depresso Espresso is pretty cool you should check them out.", 
             "<d>And also you smell bad.","Goodbye!" }));
