0ee070ae16e2027d02d6d51747273cc338944b16
ChumaDaCabbage <1707224@Mukilteo.wednet.edu>
2021-04-23 14:54:52 -0700

    Todd light only on when robot follow is off

---
+++ Assets/Evan/Scripts/Robot/RobotLightToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotLightToggle : MonoBehaviour
+{
+
+    RobotFollow rf;
+    GameObject rLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rf = gameObject.GetComponent<RobotFollow>();
+        rLight = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rf.enabled)
+        {
+            rLight.SetActive(false);
+        }
+        else
+        {
+            rLight.SetActive(true);
+        }
+    }
+}
