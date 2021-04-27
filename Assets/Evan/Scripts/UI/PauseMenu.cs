9df95b12c562a5595bfb2cf448bf5f5e2bae0051
ChumaDaCabbage <1707224@Mukilteo.wednet.edu>
2021-04-26 17:23:45 -0700

    Final pause menu

--- Assets/Evan/Scripts/UI/PauseMenu.cs
+++ Assets/Evan/Scripts/UI/PauseMenu.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     //Holds if the game is paused
     public static bool gameIsPaused = false;
 
-    //Holds reference to pause menu UI
-    public GameObject pauseMenuUI;
-
     //Holds wanted blur value
     public float wantedBlur = 1.37f;
 
-    //Holds blur-er
-    public GameObject globalVolume;
+    public float wantedPosX = 21.1f;
+    public Vector2 defaultx = new Vector2(-100, 26.2f);
 
+    public GameObject globalVolume;  //Holds blur-er
+    public GameObject buttonHolder;  //Holds holder for pause menu buttons
+    public GameObject pauseMenuUI;  //Holds reference to pause menu UI
+    public GameObject checkScreenHolder;  //Holds the checkscreen
+
+    string location;
+
+    AudioListener al;
+    RectTransform rt;
     Volume v;
     DepthOfField dOF;
 
     private void Start()
     {
+        al = gameObject.GetComponent<AudioListener>();
+
         v = globalVolume.GetComponent<Volume>();
         v.profile.TryGet(out dOF);
+
+        rt = buttonHolder.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -48,50 +59,102 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    //Resumes game
     public void Resume()
     {
-        //Resets blur
-        dOF.focusDistance.value = 1.8f;
+        if (Time.timeScale == 0f)
+        {
+            //Start sound up again
+            AudioListener.pause = false;
 
-        //Turns off pause menu UI
-        pauseMenuUI.SetActive(false);
-        //Starts time
-        Time.timeScale = 1f;
-        //Sets gameIsPaused to false
-        gameIsPaused = false;
+            //Turn of check screen
+            checkScreenHolder.SetActive(false);
+
+            //Turn on time
+            Time.timeScale = 1f;
+
+            //Sets gameIsPaused to false
+            gameIsPaused = false;
+
+            //Start blur out and slide out
+            StartCoroutine(slowBlurOut());
+            StartCoroutine(slideOut());
+        }
     }
 
+    //Pauses game
     void Pause()
     {
-        //Start slow blur
-        StartCoroutine(slowBlurIn());
+        if (Time.timeScale == 1f)
+        {
+            //Pause sound
+            AudioListener.pause = true;
 
-        //Turns on pause menu UI
-        pauseMenuUI.SetActive(true);
-        //Sets gameIsPaused to true
-        gameIsPaused = true;
+            //Start slow blur and slide in
+            StartCoroutine(slowBlurIn());
+            StartCoroutine(slideIn());
+
+            //Turns on pause menu UI
+            pauseMenuUI.SetActive(true);
+            //Sets gameIsPaused to true
+            gameIsPaused = true;
+        }
     }
 
+    //Go to credits
     public void LoadCredits()
     {
-        //Resets Time
-        Time.timeScale = 1f;
-        //Sets gameIsPaused to true
-        gameIsPaused = false;
-        //Loads Main Menu Scene
-        SceneManager.LoadScene("CreditsScene");
+        //Logs new location
+        location = "CreditsScene";
+
+        //Shows check screen
+        checkScreenHolder.SetActive(true);
     }    
 
+    //Go to menu
     public void LoadMenu()
     {
-        //Resets Time
-        Time.timeScale = 1f;
-        //Sets gameIsPaused to true
-        gameIsPaused = false;
-        //Loads Main Menu Scene
-        SceneManager.LoadScene("TitleScene");
+        //Logs new location
+        location = "TitleScene";
+
+        //Shows check screen
+        checkScreenHolder.SetActive(true);
     }
 
+    //Quit game
+    public void Quit()
+    {
+        //Logs new location
+        location = "Quit";
+
+        //Shows check screen
+        checkScreenHolder.SetActive(true);
+    }
+
+    //Loads logged loction on check accept
+    public void checkAccept()
+    {
+        if (location == "Quit")
+        {
+            Debug.Log("Would of quit if built");
+            Application.Quit();
+        }
+        else
+        {
+            //Resets Time
+            Time.timeScale = 1f;
+            //Sets gameIsPaused to true
+            gameIsPaused = false;
+            SceneManager.LoadScene(location);
+        }
+    }
+
+    public void checkDenied()
+    {
+        checkScreenHolder.SetActive(false);
+    }
+
+    //Fades in blur
     private IEnumerator slowBlurIn()
     {
         //While blur is not low enough
@@ -103,8 +166,53 @@ public class PauseMenu : MonoBehaviour
         }
         //Set blure to wanted blur
         dOF.focusDistance.value = wantedBlur;
+    }
+
+    //Fades out blur
+    private IEnumerator slowBlurOut()
+    {
+        //While blur is not low enough
+        while (dOF.focusDistance.value < 1.8f)
+        {
+            //Increment blur value
+            dOF.focusDistance.value += 0.05f;
+            yield return new WaitForSeconds(0.01f); //Wait
+        }
+        //Set blure to wanted blur
+        dOF.focusDistance.value = 1.8f;
+    }
+
+    //Slides in ui
+    private IEnumerator slideIn()
+    {
+        //While not at wanted position
+        while (rt.anchoredPosition.x < wantedPosX)
+        {
+            //Move transform
+            rt.anchoredPosition += new Vector2(4f, 0);
+            yield return new WaitForSeconds(0.001f); //Wait
+        }
+        //Set postion to wantedx
+        rt.anchoredPosition = new Vector2(wantedPosX, rt.anchoredPosition.y);
 
         //Turn off time
         Time.timeScale = 0f;
+    }
+
+    //Slides out ui
+    private IEnumerator slideOut()
+    {
+        //While not at wanted position
+        while (rt.anchoredPosition.x > defaultx.x)
+        {
+            //Move transform
+            rt.anchoredPosition -= new Vector2(8f, 0);
+            yield return new WaitForSeconds(0.001f); //Wait
+        }
+        //Set postion to wantedx
+        rt.anchoredPosition = defaultx;
+
+        //Turns off pause menu UI
+        pauseMenuUI.SetActive(false);
     }
 }
