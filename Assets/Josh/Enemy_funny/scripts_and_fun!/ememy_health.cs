39837d0bdf9f7bda835139c61d16e33a29bb0d7c
Joshua <camachobayo.2205@gmail.com>
2021-05-03 08:34:00 -0700

    Update Grug_move.anim, grug.controller, and 4 more files... enemy almost done

---
+++ Assets/Josh/Enemy_funny/scripts_and_fun!/ememy_health.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ememy_health : MonoBehaviour
+{
+    //some bunny script ;)
+    public float maxLives;
+    private float currLives;
+
+    private bool debounce;
+    private float coolDown;
+
+    //private hitEffect hitEffect;
+    private Animator anim;
+
+    private float aniTimer;
+    private bool isDead;
+    private Vector3 tempPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+       // hitEffect = GetComponent<hitEffect>();
+        currLives = maxLives;
+        aniTimer = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Trigger" && !debounce)
+        {
+            debounce = true; //keep it from hitting twice
+            if (currLives - 1 <= 0)
+            {
+                isDead = true;
+                if (GetComponent<AstarPath>() != null)
+                {
+                    GetComponent<AstarPath>().enabled = false;
+                    GetComponent<Flippy>().enabled = false;
+                }
+                GetComponents<CircleCollider2D>()[0].enabled = false;
+                GetComponents<CircleCollider2D>()[1].enabled = false;
+                anim.enabled = true;
+               // anim.Play("Deadunga");
+                tempPos = transform.position;
+            }
+            //hitEffect.hitEffectStart();
+            currLives--;
+        }
+    }
+
+    void Update()
+    {
+        if (debounce) coolDown += Time.deltaTime;
+        if (coolDown > .45)
+        {
+            debounce = false;
+            coolDown = 0;
+        }
+        if (isDead)
+        {
+            aniTimer += Time.deltaTime;
+            transform.position = tempPos;
+        }
+        if (aniTimer > 1) Destroy(gameObject);
+    }
+    public float getLives()
+    {
+        return currLives;
+    }
+}
