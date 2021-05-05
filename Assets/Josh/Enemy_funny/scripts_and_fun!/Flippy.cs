81b053d03824d6f60d0b989a0164009c5e2d5738
Joshua <camachobayo.2205@gmail.com>
2021-05-05 07:29:29 -0700

    some change

--- Assets/Josh/Enemy_funny/scripts_and_fun!/Flippy.cs
+++ Assets/Josh/Enemy_funny/scripts_and_fun!/Flippy.cs
@@ -12,10 +12,9 @@ public class Flippy : MonoBehaviour
         if (aiPath.desiredVelocity.x >= 0.01f)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-        else if (aiPath.desiredVelocity.x <= -0.01f)
+        }else if (aiPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(1f, -1f, 1f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
         }
     }
 }
