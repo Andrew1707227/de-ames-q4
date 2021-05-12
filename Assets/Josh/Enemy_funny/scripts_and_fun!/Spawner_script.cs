a326377c5dd849405af6839f2e0175c65673d74f
Joshua <camachobayo.2205@gmail.com>
2021-05-11 17:10:45 -0700

    Update MarketablePreFabs.meta, grug.prefab, and 11 more files...

--- Assets/Josh/Enemy_funny/scripts_and_fun!/Spawner_script.cs
+++ Assets/Josh/Enemy_funny/scripts_and_fun!/Spawner_script.cs
@@ -9,7 +9,7 @@ public class Spawner_script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(UngaSpawn, SpawnToPlace.transform, transform);
+        Instantiate(UngaSpawn, SpawnToPlace.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
