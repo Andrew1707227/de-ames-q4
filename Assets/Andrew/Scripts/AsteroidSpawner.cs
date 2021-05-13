5cf24669f5070f1b017c9e2f616e4fe81ce2d6e4
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-05-13 13:29:15 -0700

    changed player idle animation, added debris field backgrounds, bug fixes

--- Assets/Andrew/Scripts/AsteroidSpawner.cs
+++ Assets/Andrew/Scripts/AsteroidSpawner.cs
@@ -14,9 +14,15 @@ public class AsteroidSpawner : MonoBehaviour {
 
     void FixedUpdate() {
         if (frameCount % fireRate == 0) {
-            Instantiate(template[Random.Range(0,template.Length)]);
+            Instantiate(template[Random.Range(0,template.Length)], new Vector3(9999,9999), new Quaternion(), transform);
             frameCount = 0;
         }
         frameCount++;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            enabled = false;
+        }
     }
 }
---
+++ Assets/Andrew/Scripts/KeyScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
