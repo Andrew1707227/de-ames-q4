6b8d599a47a87658b7015c0f1cf7279eb5b512d7
ChumaDaCabbage <1707224@Mukilteo.wednet.edu>
2021-05-17 15:47:06 -0700

    Funtional boss health bar added

--- Assets/Evan/Scripts/Spider/SpiderDamage.cs
+++ Assets/Evan/Scripts/Spider/SpiderDamage.cs
@@ -1,23 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpiderDamage : MonoBehaviour
 {
 
+    public GameObject healthBar;
+    public GameObject otherDamageBox;
+
     public int maxHealth = 15;
-    int currentHealth;
+    [HideInInspector]
+    public int currentHealth;
+
+    SpiderDamage sd;
+    Slider s;
 
     // Start is called before the first frame update
     void Start()
     {
+        sd = otherDamageBox.GetComponent<SpiderDamage>();
+        s = healthBar.GetComponent<Slider>();
+        s.maxValue = maxHealth;
+        s.value = maxHealth;
+
         currentHealth = maxHealth;
+        sd.currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHealth < s.value)
+        {
+            s.value -= 0.05f;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,12 +42,12 @@ public class SpiderDamage : MonoBehaviour
         if (collision.transform.tag == "Bullets")
         {
             currentHealth--;
+            sd.currentHealth--;
 
             if (currentHealth <= 0)
             {
                 Debug.Log("Dead");
             }
-            Debug.Log("Ow!");
         }
     }
 }
