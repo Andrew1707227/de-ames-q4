977d1325b12a4cc8aa3d44fbf10fe6ccdb9c0a30
Joshua <camachobayo.2205@gmail.com>
2021-05-21 15:45:58 -0700

    Update GFX.controller, Grug.move.anim, and 4 more files... whack

--- Assets/Josh/Enemy_funny/scripts_and_fun!/Flippy.cs
+++ Assets/Josh/Enemy_funny/scripts_and_fun!/Flippy.cs
@@ -6,15 +6,22 @@ using Pathfinding;
 public class Flippy : MonoBehaviour
 {
     public AIPath aiPath;
+    SpriteRenderer Sr;
+    private void Start()
+    {
+       Sr = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
         if (aiPath.desiredVelocity.x >= 0.01f)
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            Sr.flipX = true;
+            //transform.localScale = new Vector3(-1f, 1f, 1f);
         } else if (aiPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            Sr.flipX = false;
+           //transform.localScale = new Vector3(1f, 1f, 1f);
         }
     }
 }
