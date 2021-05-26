daf8f1b93325947ff1b73a885d7c51f8a4f58108
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-05-25 18:37:26 -0700

    Update PlayerPop.cs

--- Assets/Evan/Scripts/Player/PlayerPop.cs
+++ Assets/Evan/Scripts/Player/PlayerPop.cs
@@ -109,6 +109,36 @@ public class PlayerPop : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.tag != "SafeTouch" && currentInvuln <= 0) {
+            //Start invulTimer
+            currentInvuln = invulnTime;
+
+            //Increment pops
+            currentPops--;
+
+            //Gets collision point
+            ContactPoint2D contact = collision.GetContact(0);
+
+            //Turns collision normal into Quaternion
+            Quaternion qNormal = Quaternion.LookRotation(new Vector3(0, 0, 1), contact.normal);
+
+            //Creates pop object
+            GameObject newPop = Instantiate(pop, contact.point, qNormal, pT);
+
+            //Gives pop object push direction
+            newPop.GetComponent<PopPush>().pushDirection = contact.normal;
+
+            Asource.PlayOneShot(playerDamage);
+            StartCoroutine(ChangeSprite());
+        }
+
+        //If dead and not boss dead
+        if (currentPops <= 0 && !popsDead) {
+            StartCoroutine(Respawn());
+        }
+    }
+
     private IEnumerator ChangeSprite() {
         Sprite temp = TODDSr.sprite;
         yield return new WaitForSeconds(.1f);
