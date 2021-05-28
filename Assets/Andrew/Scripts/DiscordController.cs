aa19e6968ff8c541713a1ec382426ee413acf885
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-05-27 20:25:29 -0700

    Added Discord Rich Presence Support

---
+++ Assets/Andrew/Scripts/DiscordController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Discord;
+
+public class DiscordController : MonoBehaviour {
+
+    public Discord.Discord discord;
+
+    void Start() {
+        TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+        long epoch = (long)t.TotalSeconds;
+        discord = new Discord.Discord(847250392340234290, (ulong)CreateFlags.Default);
+        var activityManager = discord.GetActivityManager();
+        var activty = new Activity {
+            Details = "In Game",
+            Timestamps = {
+                Start = epoch
+            },
+            Assets = {
+                LargeImage = "icon",
+                LargeText = "An Opportunity"
+            }
+
+        };
+        activityManager.UpdateActivity(activty,(res) => {
+            if (res == Result.Ok) Debug.Log("Discord status set.");
+            else Debug.LogWarning("Discord status failed.");
+        });
+    }
+
+    void Update() {
+        discord.RunCallbacks();
+    }
+
+    void OnApplicationQuit() {
+        discord.Dispose();
+    }
+}
