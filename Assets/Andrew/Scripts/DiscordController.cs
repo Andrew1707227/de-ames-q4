8b09cf7d5d76cfff9aed6a98d2dbc42d707c60c0
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-06-01 11:29:41 -0700

    haha try catch go brr

--- Assets/Andrew/Scripts/DiscordController.cs
+++ Assets/Andrew/Scripts/DiscordController.cs
@@ -13,35 +13,53 @@ public class DiscordController : MonoBehaviour {
         if (Application.platform == RuntimePlatform.WebGLPlayer) {
             enabled = false;
         } else {
-            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
-            long epoch = (long)t.TotalSeconds;
-            discord = new Discord.Discord(847250392340234290, (ulong)CreateFlags.Default);
-            var activityManager = discord.GetActivityManager();
-            var activty = new Activity {
-                Details = "In Game",
-                Timestamps = {
+            try {
+                TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+                long epoch = (long)t.TotalSeconds;
+                discord = new Discord.Discord(847250392340234290, (ulong)CreateFlags.Default);
+                var activityManager = discord.GetActivityManager();
+                var activty = new Activity {
+                    Details = "In Game",
+                    Timestamps = {
                 Start = epoch
             },
-                Assets = {
+                    Assets = {
                 LargeImage = "icon",
                 LargeText = "An Opportunity"
             }
 
-            };
-            activityManager.UpdateActivity(activty, (res) => {
-                if (res == Result.Ok) Debug.Log("Discord status set.");
-                else Debug.LogWarning("Discord status failed.");
-            });
+                };
+                activityManager.UpdateActivity(activty, (res) => {
+                    if (res == Result.Ok) Debug.Log("Discord status set.");
+                    else Debug.LogWarning("Discord status failed.");
+                });
+
+                activityManager.ClearActivity((result) =>
+                {
+                    if (result == Result.Ok) {
+                        Console.WriteLine("Success!");
+                    } else {
+                        Console.WriteLine("Failed");
+                    }
+                });
+
+            } catch {}
         }
     }
 
     void Update() {
-        if (Application.platform != RuntimePlatform.WebGLPlayer)
-            discord.RunCallbacks();
+        if (Application.platform != RuntimePlatform.WebGLPlayer) {
+            try {
+                discord.RunCallbacks();
+            } catch {} 
+        }
     }
 
     void OnApplicationQuit() {
-        if (Application.platform != RuntimePlatform.WebGLPlayer)
-            discord.Dispose();
+        if (Application.platform != RuntimePlatform.WebGLPlayer) {
+            try {
+                discord.Dispose();
+            } catch {} 
+        }
     }
 }
